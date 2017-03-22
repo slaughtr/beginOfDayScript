5cbfc559c9cca672036c7874c6ed0f6c8803b6da
Dallas Slaughter and jahan walsh <jahan@>
2017-03-22 15:37:25 -0700

    Changed to zsh

--- beginOfDayScript.sh
+++ beginOfDayScript.sh
@@ -1,7 +1,6 @@
-#!/bin/bash
+#!/bin/zsh
 
-zsh
-git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+git clone --recursive https://github.com/sorin-ionescu/prezto.git $HOME/.zprezto
 setopt EXTENDED_GLOB for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}" done
 
 apm install file-icons
@@ -25,4 +24,4 @@ alias gad='git add .'
 
 # git clone https://github.com/slaughtr/makeJavaProj ~/Desktop/makeJavaProj
 
-printf "\"*\":\n  editor:\n    fontSize: 18\n    scrollPastEnd: true\n    showIndentGuide: true\n    showInvisibles: true\n    softWrap: true\n  \"exception-reporting\":\n    userId: \"5a5d7ab9-6a5a-706f-c6f4-53c659d08e6c\"\n  linter:\n    errorPanelHeight: 663\n  welcome:\n    showOnStartup: false" > ~/.atom/config.cson
+printf "\"*\":\n  editor:\n    fontSize: 18\n    scrollPastEnd: true\n    showIndentGuide: true\n    showInvisibles: true\n    softWrap: true\n  \"exception-reporting\":\n    userId: \"5a5d7ab9-6a5a-706f-c6f4-53c659d08e6c\"\n  linter:\n    errorPanelHeight: 663\n  welcome:\n    showOnStartup: false" >! ~/.atom/config.cson
